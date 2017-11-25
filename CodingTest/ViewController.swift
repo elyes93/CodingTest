263926daab5442773b8509353c557a15d79cce39
elyes <elyes.zoghlami@esprit.tn>
2017-11-25 11:48:15 +0100

    Initial Commit

---
+++ CodingTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CodingTest
+//
+//  Created by Ouday Blouza on 25/11/2017.
+//  Copyright © 2017 Ouday Blouza. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
